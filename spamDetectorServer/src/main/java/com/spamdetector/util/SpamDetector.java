01b22af6b6b603c4b850ae7a1e60699741c732cb
Syeda Bisha Fatima <Syeda.fatima3@ontariotechu.net>
2024-03-04 22:11:16 -0500

    added Spam and Ham training methods and organised code

--- spamDetectorServer/src/main/java/com/spamdetector/util/SpamDetector.java
+++ spamDetectorServer/src/main/java/com/spamdetector/util/SpamDetector.java
@@ -26,6 +26,7 @@ public class SpamDetector {
         return testResults;
     }
 
+    //tests the program with spam and ham data
     public void training()
     {
         URL directory = SpamDetector.class.getClassLoader().getResource("\\data\\test");
@@ -96,34 +97,6 @@ public class SpamDetector {
         return testResults;
     }
 
-
-    //will give the probability that a file is a spam file in the testing phase
-    public double calculateProbability(File file) throws IOException
-    {
-        Set<String> words = extractWordsFromFile(file);
-        HashMap<String, Integer> spamFreqMap = new HashMap<String, Integer>();
-        HashMap<String, Integer> hamFreqMap = new HashMap<String, Integer>();
-
-        double spamProb = 0.0;
-        double hamProb = 0.0;
-
-        for (String word : words)
-        {
-
-            int spamCount = spamFreqMap.getOrDefault(word, 0);
-            int hamCount = hamFreqMap.getOrDefault(word, 0);
-
-            spamProb += Math.log((spamCount + 1.0) / (spamFreqMap.size() + 2.0));
-            hamProb += Math.log((hamCount + 1.0) / (hamFreqMap.size() + 2.0));
-
-        }
-
-        spamProb = Math.pow(Math.E, spamProb);
-        hamProb = Math.pow(Math.E, hamProb);
-
-        return spamProb / (spamProb + hamProb);
-    }
-
     //get words and their occurrences
     //calculate frequency get that for the file path given to it
     public Map<String, Integer> calculateFrequency(File directory) throws IOException {
@@ -173,6 +146,87 @@ public class SpamDetector {
     {
         return word.matches("[a-zA-Z]+");
     }
+
+    //***************************Training Spam and Ham******************************************************************
+    //trains spam data by reading from spam files and recording word frequency
+    public void trainSpam(File file, HashMap<String, Integer> SpamMap)
+    {
+        try {
+            Set<String> uniqueWords = new HashSet<>();
+            Scanner scanner = new Scanner(file); //switched over from buffered reader for variety and ease of use as Scanner is better
+
+            while (scanner.hasNext()) {
+                String word = scanner.next().toLowerCase();
+
+                if (isWord(word))
+                {
+                    if (!uniqueWords.contains(word))
+                    {
+                        uniqueWords.add(word);
+                        SpamMap.put(word, SpamMap.getOrDefault(word, 0) + 1);
+                    }
+                }
+            }
+        } catch (IOException e)
+        {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    public void trainHam(File file, HashMap<String, Integer> HamMap)
+    {
+        try {
+            Set<String> uniqueWords = new HashSet<>();
+            Scanner scanner = new Scanner(file); //switched over from buffered reader for variety and ease of use as Scanner is better
+
+            while (scanner.hasNext()) {
+                String word = scanner.next().toLowerCase();
+
+                if (isWord(word))
+                {
+                    if (!uniqueWords.contains(word))
+                    {
+                        uniqueWords.add(word);
+                        HamMap.put(word, HamMap.getOrDefault(word, 0) + 1);
+                    }
+                }
+            }
+        } catch (IOException e)
+        {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    //*********************************Probabilities******************************************************************
+    //will give the probability that a file is a spam file in the testing phase
+    public double calculateProbability(File file) throws IOException
+    {
+        Set<String> words = extractWordsFromFile(file);
+        HashMap<String, Integer> spamFreqMap = new HashMap<String, Integer>();
+        HashMap<String, Integer> hamFreqMap = new HashMap<String, Integer>();
+
+        double spamProb = 0.0;
+        double hamProb = 0.0;
+
+        for (String word : words)
+        {
+
+            int spamCount = spamFreqMap.getOrDefault(word, 0);
+            int hamCount = hamFreqMap.getOrDefault(word, 0);
+
+            spamProb += Math.log((spamCount + 1.0) / (spamFreqMap.size() + 2.0));
+            hamProb += Math.log((hamCount + 1.0) / (hamFreqMap.size() + 2.0));
+
+        }
+
+        spamProb = Math.pow(Math.E, spamProb);
+        hamProb = Math.pow(Math.E, hamProb);
+
+        return spamProb / (spamProb + hamProb);
+    }
+
 
     public static void main(String[] args)
     {
