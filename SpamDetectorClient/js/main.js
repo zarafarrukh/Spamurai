a05e084eed7e9df559bad3067ad860aa9fb0e42f
Zara Farrukh <zara.farrukh@ontariotechu.net>
2024-03-10 06:43:23 -0400

    removing extra code

--- SpamDetectorClient/js/main.js
+++ SpamDetectorClient/js/main.js
@@ -84,18 +84,3 @@ window.onscroll = () => {
   menuIcon.classList.remove('bx-x');
   navbar.classList.remove('active');
 };
-
-
-/*scrollreveal*/
-
-ScrollReveal({
-  // reset: true,
-  distance: '80px',
-  duration: 2000,
-  delay: 200
-});
-
-ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
-ScrollReveal().reveal('.home-img', { origin: 'bottom' });
-ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
-ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
